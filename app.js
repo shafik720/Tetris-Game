0c10201584ba89592048642a039da0e18d798ab2
shafik720 <shafikrasel5@gmail.com>
2022-11-14 16:02:59 +0600

    working on edge

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hellow');
+
+let grid = document.querySelector('.tetris-parent'),
+containers = Array.from(grid.querySelectorAll('div'))
+;
+
+for(let i=0; i<containers.length ; i++){
+    containers[i].innerText = i;
+}
