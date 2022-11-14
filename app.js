2b84bcc582862307e557d29b5c2a6f577abfddfe
shafik720 <shafikrasel5@gmail.com>
2022-11-14 16:08:04 +0600

    before drawing tetro

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 
 let grid = document.querySelector('.tetris-parent'),
-containers = Array.from(grid.querySelectorAll('div'))
+containers = Array.from(grid.querySelectorAll('div')),
+width = 10,
+timer,
+nextRandom = 0,
+currentRotation = 0,
+currentPosition = 0
 ;
 
 for(let i=0; i<containers.length ; i++){
     containers[i].innerText = i;
 }
+
+// lTetromino 
+const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+
+let tetro   =   [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+let random  =   Math.floor(Math.random() * tetro.length);
+let present =   tetro[random][currentRotation];
